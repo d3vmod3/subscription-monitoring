872035a1e71a531ca57e7f9fcf69b95726c50f10
Alexie Tuzon <astuzon@gmail.com>
2025-11-30 10:52:01 +0800

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        cors: true,
-        host: process.env.VITE_SERVER_HOST, // allow all network interfaces (including ZeroTier)
-        port: process.env.VITE_SERVER_PORT, // optional, you can change this if needed
-        hmr: {
-            host: process.env.VITE_SERVER_HMR_HOST, // e.g. 10.147.17.45
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            laravel({
+                input: ["resources/css/app.css", "resources/js/app.js"],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            cors: true,
+            host: env.VITE_SERVER_HOST,
+            port: env.VITE_SERVER_PORT,
+            hmr: {
+                host: env.VITE_SERVER_HMR_HOST,
+            },
         },
-    },
+    };
 });
