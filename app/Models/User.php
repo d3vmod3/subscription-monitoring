c87e89513c5dd1600c5c9c057cf09732cd75868a
Alexie Tuzon <astuzon@gmail.com>
2026-06-08 16:59:27 +0800

    upgraded to laravel 13

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles, HasApiTokens;
 
     protected $fillable = [
         'first_name',
